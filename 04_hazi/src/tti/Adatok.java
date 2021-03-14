54cc5dea644a02bc1c88d35114a44fdc96a4e394
szaboobaalint <78734022+szaboobaalint@users.noreply.github.com>
2021-03-14 19:52:58 +0100

    commit

--- 04_hazi/src/tti/Adatok.java
+++ 04_hazi/src/tti/Adatok.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Adatok {
 	public static void main(String[] args) {
 		Szemely szem = new Szemely();
-		//szem.setNev("János");
-		//szem.setSuly(70);
-		//szem.setMagassag(1.8);
-		szem.Testtomegindex(szem.getSuly(), szem.getMagassag());
+		szem.setNev("János");
+		szem.setSuly(70);
+		szem.setMagassag(1.8);
+		/*szem.Testtomegindex(szem.getSuly(), szem.getMagassag());
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Adjon meg egy nevet: ");
 		String nev = sc.nextLine();
 		System.out.println("Adjon meg egy testtomeget(kg-ban egeszre kerekitve): ");
 		int suly = sc.nextInt();
 		System.out.println("Adjon meg egy magassagot(meterben): ");
-		double magassag = sc.nextDouble();
+		double magassag = sc.nextDouble();*/
 		
 		System.out.println(szem.toString());
 
